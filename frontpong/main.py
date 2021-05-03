120d80ec37b49c75f133d14324566f03b3d41dd7
NovelleP <alberto109@hotmail.es>
2021-05-03 05:14:25 +0200

    refactor

--- frontpong/main.py
+++ frontpong/main.py
@@ -8,8 +8,6 @@ if __name__ == '__main__':
     window = pyglet.window.Window()
     racket = Racket(speed_x=0, speed_y=350, window=window, x=15, y=window.height - 100, width=5, height=100, color=(50, 225, 30))
     ball = Ball(speed_x=150, speed_y=150, window=window, racket=racket, x=100, y=150, radius=10, color=(50, 225, 30))
-    keyboard = pyglet.window.key.KeyStateHandler()
-    window.push_handlers(keyboard)
     pyglet.clock.schedule_interval(lambda dt: racket.update(keys, dt), 1 / 120.0)
     pyglet.clock.schedule_interval(ball.update, 1 / 120.0)
     pyglet.clock.schedule_interval(lambda _: ball.update_speed_by_factor(0.2), 2)
